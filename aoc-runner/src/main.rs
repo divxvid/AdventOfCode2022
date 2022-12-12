c53344cb833c9acd2f00a8dac69a4a5882c62951
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2022-12-12 14:06:03 +0530

    Add timing function to aoc runner

--- aoc-runner/src/main.rs
+++ aoc-runner/src/main.rs
@@ -2,6 +2,7 @@
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use std::time::SystemTime;
 
 //Custom imports
 use day_12;
@@ -20,8 +21,17 @@ fn read_input_from_file(file_name: &str) -> String {
     buffer
 }
 
+fn timeit<F: Fn() -> T, T>(f: F) -> T {
+    let start = SystemTime::now();
+    let result = f();
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+    println!("Time Taken: {} ms.", duration.as_millis());
+    result
+}
+
 fn main() {
     let file_data = read_input_from_file(INPUT_FILE_NAME);
-    println!("Part one output:\n{}", day_12::part_one(&file_data));
-    println!("Part two output:\n{}", day_12::part_two(&file_data));
+    println!("Part one output:\n{}", timeit(|| day_12::part_one(&file_data)));
+    println!("Part two output:\n{}", timeit(|| day_12::part_two(&file_data)));
 }
